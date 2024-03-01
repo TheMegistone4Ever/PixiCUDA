47ec31ffde59433db49b3fa0119adb385f58223e
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2024-03-01 22:47:38 +0200

    Done the Motion Blur procedure - single thread

--- PixiCUDA/src/kernel.cu
+++ PixiCUDA/src/kernel.cu
@@ -7,266 +7,137 @@
 
 #ifdef __INTELLISENSE__
 #include "cualgo/intellisense/intellisense_cuda_intrinsics.hpp"
-#endif
+#endif // __INTELLISENSE__
 
-#define DEG_TO_RAD 0.01745329252f
+constexpr auto BYTE_SIZE = 8;
+#define BIT_VECTOR_SIZE (MAX_KERNEL_SIZE * MAX_KERNEL_SIZE / BYTE_SIZE + (MAX_KERNEL_SIZE * MAX_KERNEL_SIZE % BYTE_SIZE != 0))
 
-// Function to apply motion blur to an image
+__constant__ unsigned char device_kernel[BIT_VECTOR_SIZE] = { 0 };
+
+__host__ void setBit(unsigned char* bvec, const int index)
+{
+    bvec[index / BYTE_SIZE] |= 1 << (index % BYTE_SIZE);
+}
+
+__device__ bool testBit(const unsigned char* bvec, const int index)
+{
+    return bvec[index / BYTE_SIZE] & (1 << (index % BYTE_SIZE));
+}
+
 __global__ void motion_blur_cuda(
-	const unsigned char* in_image,
-	unsigned char* out_image,
-	const unsigned char* kernel,
-	const int kernel_size,
-	const int channels,
-	const int height,
-	const int width,
-	const float angle_deg,
-	const int distance
+    const unsigned char* in_image,
+    unsigned char* out_image,
+    const int kernel_size,
+    const int channels,
+    const int height,
+    const int width,
+    const float angle_deg,
+    const int distance
 );
 
 void cuda_motion_blur_image(
-	const unsigned char* in_image,
-	unsigned char* out_image,
-	const float angle_deg,
-	const int distance,
-	const int height,
-	const int width,
-	const int channels
-) {
-	// Calculate the size of the image
-	const size_t image_size = height * width * channels * sizeof(unsigned char);
-	
-	// Create the kernel and fill it with the correct values
+    const unsigned char* in_image,
+    unsigned char* out_image,
+    const float angle_deg,
+    const int distance,
+    const int height,
+    const int width,
+    const int channels
+)
+{
+    // Calculate the size of the image
+    const size_t image_size = height * width * channels * sizeof(unsigned char);
 
-	float angle_rad = angle_deg * DEG_TO_RAD;
-	
-	int kernel_size = distance * 2 + 1; // +1 to include the center pixel
-	int grid_kernel_size = kernel_size * kernel_size;
-	//size_t kernel_size = grid_kernel_size * sizeof(float);
+    // Create the kernel and fill it with the correct values
+    float angle_rad = angle_deg * DEG_TO_RAD;
+    int kernel_size = distance * 2 + 1; // +1 to include the center pixel
+    unsigned char* host_kernel = new unsigned char[BIT_VECTOR_SIZE] { 0 };
 
-	unsigned char* kernel = new unsigned char[grid_kernel_size] { 0 };
+    for (int i = 0; i < kernel_size; i++)
+    {
+        int x = distance + int(i * cos(angle_rad));
+        int y = distance + int(i * sin(angle_rad));
+        if (x >= 0 && x < kernel_size && y >= 0 && y < kernel_size)
+        {
+            setBit(host_kernel, y * kernel_size + x);
+        }
+    }
 
-	for (int i = 0; i < kernel_size; i++) {
-		int x = distance + int(i * cos(angle_rad));
-		int y = distance + int(i * sin(angle_rad));
-		if (x < 0 || x >= kernel_size || y < 0 || y >= kernel_size) {
-			break;
-		}
-		kernel[y * kernel_size + x] = 1;
-	}
+    // Copy the host kernel data to the device constant memory
+    cudaMemcpyToSymbol(device_kernel, host_kernel, BIT_VECTOR_SIZE);
 
-	// Printf the kernel
-	//printf("Kernel:\n");
-	//for (int i = 0; i < grid_kernel_size; i++) {
-	//	if (kernel[i] > 0) {
-	//		printf("X ");
-	//	}
-	//	else {
-	//		printf("  ");
-	//	}
-	//	if ((i + 1) % kernel_size == 0) {
-	//		printf("\n");
-	//	}
-	//}
+    // Allocate CUDA variable memory on the device
+    unsigned char* device_in_image = nullptr;
+    unsigned char* device_out_image = nullptr;
 
-	// Allocate CUDA variable memory on the device
-	unsigned char* device_in_image = nullptr;
-	unsigned char* device_out_image = nullptr;
+    cudaMalloc((void**)&device_in_image, image_size);
+    cudaMalloc((void**)&device_out_image, image_size);
+    cudaMemset(device_out_image, NULL, image_size);
 
-	cudaMalloc((void**)&device_in_image, image_size);
-	cudaMalloc((void**)&device_out_image, image_size);
-	cudaMemset(device_out_image, NULL, image_size);
+    // Copy the host variables to the device (CPU -> GPU)
+    cudaMemcpy(device_in_image, in_image, image_size, cudaMemcpyHostToDevice);
 
-	// Copy the host variables to the device (CPU -> GPU)
-	cudaMemcpy(device_in_image, in_image, image_size, cudaMemcpyHostToDevice);
+    // Kernel launch
+    dim3 grid_image(width, height);
+    motion_blur_cuda << <grid_image, 1 >> > (device_in_image, device_out_image, kernel_size, channels, height, width, angle_deg, distance);
 
-	// Kernel launch
-	dim3 grid_image(width, height);
-	motion_blur_cuda << <grid_image, 1 >> > (device_in_image, device_out_image, kernel, kernel_size, channels, height, width, angle_deg, distance);
+    // Copy the device variables to the host (GPU -> CPU)
+    cudaMemcpy(out_image, device_out_image, image_size, cudaMemcpyDeviceToHost);
 
-	// Copy the device variables to the host (GPU -> CPU)
-	cudaMemcpy(out_image, device_out_image, image_size, cudaMemcpyDeviceToHost);
-
-	// Free up the memory on the device (GPU)
-	cudaFree(device_in_image);
-	cudaFree(device_out_image);
+    // Free up the memory on the device (GPU)
+    cudaFree(device_in_image);
+    cudaFree(device_out_image);
 }
 
 __global__ void motion_blur_cuda(
-	const unsigned char* in_image,
-	unsigned char* out_image,
-	const unsigned char* kernel,
-	const int kernel_size,
-	const int channels,
-	const int height,
-	const int width,
-	const float angle_deg,
-	const int distance
-) {
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	int index = (x + y * width) * channels;
+    const unsigned char* in_image,
+    unsigned char* out_image,
+    const int kernel_size,
+    const int channels,
+    const int height,
+    const int width,
+    const float angle_deg,
+    const int distance
+)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int index = (x + y * width) * channels;
 
-	int grid_kernel_size = kernel_size * kernel_size;
-	float angle_rad = angle_deg * DEG_TO_RAD;
-	unsigned char* kernel = new unsigned char[grid_kernel_size] { 0 };
-	for (int i = 0; i < kernel_size; i++) {
-		int x = distance + int(i * cos(angle_rad));
-		int y = distance + int(i * sin(angle_rad));
-		if (x < 0 || x >= kernel_size || y < 0 || y >= kernel_size) {
-			break;
-		}
-		kernel[y * kernel_size + x] = 1;
-	}
+    if (x < width && y < height)
+    {
+        int start_kernel_x = x - kernel_size / 2;
+        int start_kernel_y = y - kernel_size / 2;
 
-	if (x < width && y < height) {
-		int start_kernel_x = x - kernel_size / 2;
-		int start_kernel_y = y - kernel_size / 2;
-		
-		for (int channel = 0; channel < channels; channel++) {
-			long channel_sum = 0;
-			for (int x_kernel = start_kernel_x; x_kernel < start_kernel_x + kernel_size; x_kernel++) {
-				if (x_kernel < 0 || x_kernel >= width) {
-					continue;
-				}
+        for (int channel = 0; channel < channels; channel++)
+        {
+            double channel_sum = 0;
+            for (int x_kernel = start_kernel_x; x_kernel < start_kernel_x + kernel_size; x_kernel++)
+            {
+                if (x_kernel < 0 || x_kernel >= width)
+                {
+                    continue;
+                }
 
-				for (int y_kernel = start_kernel_y; y_kernel < start_kernel_y + kernel_size; y_kernel++) {
-					if (y_kernel < 0 || y_kernel >= height) {
-						continue;
-					}
-					
-					unsigned char pixel = in_image[(x_kernel + y_kernel * width) * channels + channel];
+                for (int y_kernel = start_kernel_y; y_kernel < start_kernel_y + kernel_size; y_kernel++)
+                {
+                    if (y_kernel < 0 || y_kernel >= height)
+                    {
+                        continue;
+                    }
 
-					int kernel_index = (x_kernel - start_kernel_x) + (y_kernel - start_kernel_y) * kernel_size;
-					unsigned char kernel_value = kernel[(x_kernel - start_kernel_x) + (y_kernel - start_kernel_y) * kernel_size];
+                    unsigned char pixel = in_image[(x_kernel + y_kernel * width) * channels + channel];
 
-					if (kernel_value > 0) {
-						channel_sum += pixel;
-					}
-				}
-			}
-			out_image[index + channel] = channel_sum / (distance + 1);
-		}
-	}
+                    int kernel_index = (x_kernel - start_kernel_x) + (y_kernel - start_kernel_y) * kernel_size;
+                    unsigned char kernel_value = testBit(device_kernel, kernel_index);
 
-	delete[] kernel;
+                    if (kernel_value)
+                    {
+                        channel_sum += static_cast<double>(pixel) / (distance + 1);
+                    }
+                }
+            }
+            out_image[index + channel] = static_cast<unsigned char>(channel_sum);
+        }
+    }
 }
-
-//__global__ void negative_cuda(
-//	unsigned char* image,
-//	const int channels
-//);
-//
-//void cuda_negative_image(
-//	unsigned char* image,
-//	const int height,
-//	const int width,
-//	const int channels
-//) {
-//	const int image_size = height * width * channels * sizeof(unsigned char);
-//
-//	unsigned char* device_image = nullptr;
-//
-//	// Allocate CUDA variable memory on the device
-//	cudaMalloc((void**)&device_image, image_size);
-//
-//	// Copy the host variables to the device (CPU -> GPU)
-//	cudaMemcpy(device_image, image, image_size, cudaMemcpyHostToDevice);
-//
-//	// Kernel launch
-//	dim3 grid_image(width, height);
-//	negative_cuda << <grid_image, 1 >> > (device_image, channels);
-//
-//	// Copy the device variables to the host (GPU -> CPU)
-//	cudaMemcpy(image, device_image, image_size, cudaMemcpyDeviceToHost);
-//
-//	// Free up the memory on the device (GPU)
-//	cudaFree(device_image);
-//}
-//
-//__global__ void negative_cuda(
-//	unsigned char* image,
-//	const int channels
-//) {
-//	int x = blockIdx.x * blockDim.x + threadIdx.x;
-//	int y = blockIdx.y * blockDim.y + threadIdx.y;
-//	int index = (x + y * gridDim.x) * channels;
-//
-//	for (int i = 0; i < channels; i++) {
-//		image[index + i] = 255 - image[index + i];
-//	}
-//}
-//
-//__global__ void histogram_cuda(
-//    const unsigned char* image,
-//    const int width,
-//    const int height,
-//    const int channels,
-//    int* histogram_r,
-//    int* histogram_g,
-//    int* histogram_b
-//);
-//
-//void cuda_calculate_histogram(
-//    const unsigned char* image,
-//    const int height,
-//    const int width,
-//    const int channels,
-//    int* histogram_r,
-//    int* histogram_g,
-//    int* histogram_b
-//) {
-//    const int image_size = height * width * channels * sizeof(unsigned char);
-//    const int histogram_size = 256 * sizeof(int);
-//
-//    unsigned char* device_image = nullptr;
-//    int* device_histogram_r = nullptr;
-//    int* device_histogram_g = nullptr;
-//    int* device_histogram_b = nullptr;
-//
-//    // Allocate CUDA variable memory on the device
-//    cudaMalloc((void**)&device_image, image_size);
-//    cudaMalloc((void**)&device_histogram_r, histogram_size);
-//    cudaMalloc((void**)&device_histogram_g, histogram_size);
-//    cudaMalloc((void**)&device_histogram_b, histogram_size);
-//
-//    // Copy the host variables to the device (CPU -> GPU)
-//    cudaMemcpy(device_image, image, image_size, cudaMemcpyHostToDevice);
-//    cudaMemcpy(device_histogram_r, histogram_r, histogram_size, cudaMemcpyHostToDevice);
-//    cudaMemcpy(device_histogram_g, histogram_g, histogram_size, cudaMemcpyHostToDevice);
-//    cudaMemcpy(device_histogram_b, histogram_b, histogram_size, cudaMemcpyHostToDevice);
-//
-//    // Kernel launch
-//    dim3 grid_image(width, height);
-//    dim3 block_image(1, 1);
-//    histogram_cuda << <grid_image, block_image >> > (device_image, width, height, channels, device_histogram_r, device_histogram_g, device_histogram_b);
-//
-//    // Copy the device variables to the host (GPU -> CPU)
-//    cudaMemcpy(histogram_r, device_histogram_r, histogram_size, cudaMemcpyDeviceToHost);
-//    cudaMemcpy(histogram_g, device_histogram_g, histogram_size, cudaMemcpyDeviceToHost);
-//    cudaMemcpy(histogram_b, device_histogram_b, histogram_size, cudaMemcpyDeviceToHost);
-//
-//    // Free up the memory on the device (GPU)
-//    cudaFree(device_image);
-//    cudaFree(device_histogram_r);
-//    cudaFree(device_histogram_g);
-//    cudaFree(device_histogram_b);
-//}
-//
-//__global__ void histogram_cuda(
-//    const unsigned char* image,
-//    const int width,
-//    const int height,
-//    const int channels,
-//    int* histogram_r,
-//    int* histogram_g,
-//    int* histogram_b
-//) {
-//    int x = blockIdx.x * blockDim.x + threadIdx.x;
-//    int y = blockIdx.y * blockDim.y + threadIdx.y;
-//    int index = (y * gridDim.x + x) * channels;
-//
-//    atomicAdd(&histogram_r[image[index + 0]], 1);
-//    atomicAdd(&histogram_g[image[index + 1]], 1);
-//    atomicAdd(&histogram_b[image[index + 2]], 1);
-//}
--- PixiCUDA/src/main.cpp
+++ PixiCUDA/src/main.cpp
@@ -13,11 +13,13 @@
 * * test - tests files if you write tests (indefinitely you should).
 * * doc - documentation files.
 * */
-#define DEG_TO_RAD 0.01745329252f
-#define RED_BOLD "\033[1;31m"
-#define GREEN_BOLD "\033[1;32m"
-#define BLUE_BOLD "\033[1;34m"
-#define RESET_COLOR "\033[0m"
+
+#define MAX_DISTANCE (MAX_KERNEL_SIZE / 2 - 1)
+constexpr unsigned short MAX_ANGLE_DEG = 360;
+constexpr unsigned char ESC_KEY = 27;
+constexpr unsigned char WAIT_TIME = 50;
+constexpr unsigned short WIN_WIDTH = 1280;
+constexpr unsigned short WIN_HEIGHT = 720;
 
 #include <iostream>
 #include <chrono>
@@ -28,8 +30,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
-//#include "cualgo/cuda_histogram.h"
-//#include "cualgo/negative.cuh"
 #include "cualgo/motion_blur.cuh"
 
 using namespace std;
@@ -38,82 +38,76 @@ namespace fs = filesystem;
 
 typedef chrono::high_resolution_clock::time_point TimeVar;
 
+static void onTrackbarAngle(int angle, void* userdata)
+{
+    *(float*)userdata = static_cast<float>(angle);
+}
+
+static void onTrackbarDistance(int distance, void* userdata)
+{
+    *(unsigned int*)userdata = static_cast<unsigned int>(distance);
+}
+
 int main(int argc, char** argv)
 {
-	string images_path = R"(C:\Users\megis\VSProjects\PixiCUDA\PixiCUDA\images)";
-	string image_name = R"(ny_ts_cropp_macdonalds.png)";
+    string images_path = R"(C:\Users\megis\VSProjects\PixiCUDA\PixiCUDA\images)";
+    string image_name = R"(ny_ts_cropp_macdonalds.png)";
 
-	fs::path image_path = fs::path(images_path) / image_name;
+    fs::path image_path = fs::path(images_path) / image_name;
 
-	//float angle_deg = 45;
-	//int distance = 5;
-	//
-	//float* kernel = new float[(distance * 2 + 1) * (distance * 2 + 1)];
-	//
-	//int grid_kernel_size = fill_k(kernel, angle_deg, distance);
-	//
-	//Mat kernel_image = Mat(distance * 2 + 1, distance * 2 + 1, CV_32F, kernel);
-	
-	cout << "Image path: " << image_path << "\n";
-	if (!fs::exists(image_path))
-	{
-		cout << "Could not open or find the image..." << endl;
-		return -1;
-	}
-	Mat image = imread(image_path.string());
+    cout << "Image path: " << image_path << "\n";
+    if (!fs::exists(image_path))
+    {
+        cout << "Could not open or find the image..." << endl;
+        return -1;
+    }
+    Mat image = imread(image_path.string());
 
-	// Image properties
-	cout << "\tImage height: " << image.rows << "\n";
-	cout << "\tImage width: " << image.cols << "\n";
-	cout << "\tImage channels: " << image.channels() << "\n";
+    // Image properties
+    cout << "\tImage height: " << image.rows << "\n";
+    cout << "\tImage width: " << image.cols << "\n";
+    cout << "\tImage channels: " << image.channels() << "\n";
 
-	// Motion Blur Image
-	Mat motion_blur_image = image.clone();
-	cuda_motion_blur_image(
-		image.data,
-		motion_blur_image.data,
-		30.5,
-		20,
-		image.rows,
-		image.cols,
-		image.channels()
-	);
-	imwrite("motion_blur_image.png", motion_blur_image);
-	imshow("Motion Blur Image", motion_blur_image);
-	waitKey(0);
-	destroyAllWindows();
+    float angle_deg;
+    unsigned int distance;
 
-	//cuda_negative_image(image.data, image.rows, image.cols, image.channels());
-	//imwrite("negative_image.png", image);
-	//int histogram_r[256] = { 0 };
-	//int histogram_g[256] = { 0 };
-	//int histogram_b[256] = { 0 };
-	//
-	//cuda_calculate_histogram(image.data, image.rows, image.cols, image.channels(), histogram_r, histogram_g, histogram_b);
-	//
-	//int total_pixels = image.rows * image.cols;
-	//int total_pixels_histogram = 0;
-	//
-	//cout << "histogram: \n";
-	//cout << "value\tR\tG\tB\n" << "—\t—\t—\t—\n";
-	//for (int i = 0; i < sizeof(histogram_r) / sizeof(histogram_r[0]); i++)
-	//{
-	//	cout << i << ":";
-	//	cout << "\t|" << RED_BOLD << histogram_r[i] << RESET_COLOR;
-	//	cout << "\t|" << GREEN_BOLD << histogram_g[i] << RESET_COLOR;
-	//	cout << "\t|" << BLUE_BOLD << histogram_b[i] << RESET_COLOR;
-	//	cout << "\n";
-	//	total_pixels_histogram += histogram_r[i] + histogram_g[i] + histogram_b[i];
-	//}
-	//total_pixels_histogram /= image.channels();
-	//
-	//cout << "Total pixels: " << total_pixels << "\n";
-	//cout << "Total pixels histogram: " << total_pixels_histogram << "\n";
-	//cout << "Remaining pixels: " << total_pixels - total_pixels_histogram << "\n";
-	//
-	////imshow("Original Image", image);
-	////waitKey(0);
-	////destroyAllWindows();
+    // Create a window with a stack of images
+    namedWindow("Motion Blur", WINDOW_NORMAL);
+    resizeWindow("Motion Blur", WIN_WIDTH, WIN_HEIGHT);
 
-	return 0;
+    // Create trackbars
+    createTrackbar("Angle", "Motion Blur", 0, MAX_ANGLE_DEG, onTrackbarAngle, &angle_deg);
+    createTrackbar("Distance", "Motion Blur", 0, MAX_DISTANCE, onTrackbarDistance, &distance);
+
+    // Motion Blur Image
+    Mat motion_blur_image = image.clone();
+    Mat stacked_image;
+    hconcat(image, motion_blur_image, stacked_image);
+    imshow("Motion Blur", stacked_image);
+
+    // Loop to handle events
+    while (true)
+    {
+        cuda_motion_blur_image(
+            image.data,
+            motion_blur_image.data,
+            angle_deg,
+            distance,
+            image.rows,
+            image.cols,
+            image.channels()
+        );
+        hconcat(image, motion_blur_image, stacked_image);
+        imshow("Motion Blur", stacked_image);
+
+        char key = waitKey(WAIT_TIME);
+        if (key == ESC_KEY)
+        {
+            break;
+        }    
+    }
+
+    destroyAllWindows();
+
+    return 0;
 }
